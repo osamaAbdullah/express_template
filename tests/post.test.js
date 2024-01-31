8f5a63849d30b098a40ce918c951ce127b8e225c
osama_akre <osama@akre.com>
2024-02-01 00:08:16 +0300

    tests refactored

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,59 +1,58 @@
-import request from 'supertest'
-import app from '#root/app'
-import { sequelize } from "#db/index";
-import { createPost, makePost } from "#db/factories/post.factory";
-import { Post } from "#db/models/post.model";
+import request from 'supertest';
+import app from '#root/app';
+import { sequelize } from '#db/index';
+import { createPost, makePost } from '#db/factories/post.factory';
+import { Post } from '#db/models/post.model';
 
-const basePath = "/api/v1/posts/";
+const basePath = '/api/v1/posts/';
 
-describe("test post model", () => {
+describe('test post model', () => {
 
     beforeAll(async () => {
-        await sequelize.sync({ force: true })
-    })
+        await sequelize.sync({ force: true });
+    });
 
-    it("creates a post", async () => {
+    it('creates a post', async () => {
         const post = await makePost();
         const res = await request(app).post(basePath).send(post);
-        expect(res.statusCode).toBe(200)
-        expect(res.body.title).toMatch(post.title)
+        expect(res.statusCode).toBe(200);
+        expect(res.body.title).toMatch(post.title);
     });
 
-    it("get a specific post in db", async () => {
+    it('get a specific post in db', async () => {
         const post = await createPost();
         const res = await request(app).get(basePath + post.id);
-        expect(res.statusCode).toBe(200)
-        expect(res.body.title).toMatch(post.title)
+        expect(res.statusCode).toBe(200);
+        expect(res.body.title).toMatch(post.title);
     });
 
-    it("update a specific post in db", async () => {
+    it('update a specific post in db', async () => {
         const post = await createPost();
         const title = 'new title';
-        console.log({ ...post, title });
         const res = await request(app).put(basePath + post.id).send({ ...post, title });
-        expect(res.statusCode).toBe(200)
-        expect(res.body.title).toMatch(title)
+        expect(res.statusCode).toBe(200);
+        expect(res.body.title).toMatch(title);
     });
 
-    it("get all post in db", async () => {
+    it('get all post in db', async () => {
         await Post.truncate();
-        const postIds = await createPost({}, 3)
+        const postIds = await createPost({}, 3);
         const res = await request(app).get(basePath);
-        expect(res.statusCode).toBe(200)
-        expect(res.body.posts.map(post => post.id)).toEqual(postIds)
+        expect(res.statusCode).toBe(200);
+        expect(res.body.posts.map(post => post.id)).toEqual(postIds);
     });
 
-    it("deletes a specific post in db", async () => {
+    it('deletes a specific post in db', async () => {
         await Post.truncate();
         const post = await createPost();
-        expect(await Post.count()).toEqual(1)
-        const res = await request(app).delete(basePath + post.id)
-        expect(res.statusCode).toBe(200)
-        expect(await Post.count()).toEqual(0)
+        expect(await Post.count()).toEqual(1);
+        const res = await request(app).delete(basePath + post.id);
+        expect(res.statusCode).toBe(200);
+        expect(await Post.count()).toEqual(0);
     });
 
     afterAll(async () => {
-        await sequelize.close()
-    })
+        await sequelize.close();
+    });
 
 });
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,69 +1,58 @@
-import request from 'supertest'
-import app from '#root/app'
-import { sequelize } from "#db/index";
+import request from 'supertest';
+import app from '#root/app';
+import { sequelize } from '#db/index';
+import { createUser, makeUser } from '#db/factories/user.factory';
+import { User } from '#models/user.model';
 
-const user = {
-    id: 1,
-    name: 'Osama',
-    email: 'Osama@gmail.com',
-    role: 'normalUser',
-}
-const user2 = {
-    id: 2,
-    name: 'OsamaA',
-    email: 'OsamaA@gmail.com',
-    role: 'admin',
-}
+const basePath = '/api/v1/users/';
 
 describe('test user model', () => {
 
     beforeAll(async () => {
-        await sequelize.sync({ force: true })
-    })
-
-    it("creates a user", async () => {
-        const res = await request(app).post("/api/v1/users").send(user);
-        expect(res.statusCode).toBe(200)
-        expect(res.body.name).toMatch(user.name)
+        await sequelize.sync({ force: true });
     });
 
-    it("get a specific user in db", async () => {
-        const res = await request(app).get("/api/v1/users/" + user.id);
-        expect(res.statusCode).toBe(200)
-        expect(res.body.name).toMatch(user.name)
+    it('creates a user', async () => {
+        const user = await makeUser();
+        const res = await request(app).post(basePath).send(user);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.name).toMatch(user.name);
     });
 
-    it("update a specific user in db", async () => {
-        const res = await request(app).put("/api/v1/users/" + user.id).send({ ...user, name: 'updated' });
-        expect(res.statusCode).toBe(200)
-        expect(res.body.name).toMatch('updated')
+    it('get a specific user in db', async () => {
+        const user = await createUser();
+        const res = await request(app).get(basePath + user.id);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.name).toMatch(user.name);
     });
 
-    it("get all users in db", async () => {
-        await request(app).post("/api/v1/users").send(user2)
-        const res = await request(app).get("/api/v1/users");
-        expect(res.statusCode).toBe(200)
-        expect(res.body.users.length).toEqual(2)
+    it('update a specific user in db', async () => {
+        const user = await createUser();
+        const name = 'name updated';
+        const res = await request(app).put(basePath + user.id).send({ ...user, name });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.name).toMatch(name);
     });
 
-    it("deletes a specific user in db", async () => {
-        const res = await request(app).delete("/api/v1/users/" + user.id)
-        expect(res.statusCode).toBe(200)
+    it('get all users in db', async () => {
+        await sequelize.sync({ force: true });
+        const userIds = await createUser({}, 3);
+        const res = await request(app).get(basePath);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.users.map(user => user.id)).toEqual(userIds);
+    });
 
-        const res2 = await request(app).get("/api/v1/users");
-        expect(res2.statusCode).toBe(200)
-        expect(res2.body.users.length).toEqual(1)
-
-        const res3 = await request(app).delete("/api/v1/users/" + user2.id)
-        expect(res3.statusCode).toBe(200)
-
-        const res4 = await request(app).get("/api/v1/users");
-        expect(res4.statusCode).toBe(200)
-        expect(res4.body.users.length).toEqual(0)
+    it('deletes a specific user in db', async () => {
+        await sequelize.sync({ force: true });
+        const user = await createUser();
+        expect(await User.count()).toEqual(1);
+        const res = await request(app).delete(basePath + user.id);
+        expect(res.statusCode).toBe(200);
+        expect(await User.count()).toEqual(0);
     });
 
     afterAll(async () => {
-        await sequelize.close()
-    })
+        await sequelize.close();
+    });
 
 });
