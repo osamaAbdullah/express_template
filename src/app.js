466344a210788f890cbb44eb5b0f482f41486751
osama_akre <osama@akre.com>
2024-02-05 16:34:05 +0300

    init branch

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ import 'dotenv/config';
 import { URL } from "url";
 import { globalErrorHandlers, norFoundErrorHandler } from "#root/exceptions/errorHandlers";
 import '#root/exceptions/asyncErrorHandling';
+import session from 'express-session'
+
 
 import APIRoutes from '#routes/api'
 
 const __dirname = new URL(".", import.meta.url).pathname;
 
 const app = express();
+
+app.use(session({
+    secret: 'ajnsdfkjnaskjfnjk8978998*&(*^(*($E*(',
+    resave: false
+}));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
